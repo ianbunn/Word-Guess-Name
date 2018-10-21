2ccca6b49bac14efbc5c11999641206911557c92
ianbunn <ianrichardbunn@gmail.com>
2018-10-20 22:53:07 -0500

    added finishing touches to mechanics and style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,74 @@
-document.onkeyup = function(event) {
-    hangman.gameOn = true;
-    if (hangman.gameOn == true) {
-        hangman.instructions1.style.display = "none";
-        hangman.instructions2.style.display = "block";
+var robots = ["r2d2", "c3po", "robocop", "bender", "terminator", "robby", "rosie", "megatron", "t1000", "linguo", "sonny", "vision", "alpha", "bb8"];
+var robot;
+var underscores;
+var wins = 0;
+var guessedLetters = [];
+var guessesLeft;
+var gameOn = false;
+
+function newGame(event) {
+    gameOn = true;
+    robot = robots[Math.floor(Math.random() * robots.length)];
+    underscores = [];
+    guessedLetters = [];
+    guessesLeft = 10;
+    for (var i = 0; i < robot.length; i++) {
+        underscores.push("_");
     }
-    hangman.keyPressed = event.key.toLowerCase();
-
-    hangman.robot = hangman.famousRobots[Math.floor(Math.random() * hangman.famousRobots.length)]
-    // console.log(hangman.robot)
-
-    hangman.robotLetters = hangman.robot.split(",")
-    // console.log(hangman.robotLetters)
-    
+    document.querySelector("#wins").innerHTML = wins;
+    document.querySelector("#currentWord").innerHTML = underscores.join(" ");
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
 }
 
+document.onkeyup = function (event) {
+
+    // Start game and hide instructions
+    if (event.keyCode == 32 && gameOn == false) {
+        newGame()
+        gameOn == true
+        document.getElementById("instructions").style.display = "none";
+    }
+
+    // Saving the keyCode in var userGuess to compare against robot
+    if(event.keyCode >= 48 && event.keyCode <= 90 || event.keyCode >=96 && event.keyCode <= 105) {
+        var userGuess = event.key.toLowerCase();
+    }
+
+    // Loop through the robot and if there's a match it replaces the underscore with the matched letter
+    var letterFound = false;
+    for (var i = 0; i < robot.length; i++) {
+        if (robot[i] === userGuess) {
+            underscores[i] = robot[i];
+            letterFound = true;
+        }
+    }
+
+    // if userGuess doesn't match it pushes the letter to the guessedLetters and deducts from guessesLeft
+    if (letterFound === false && !guessedLetters.includes(userGuess)) {
+        guessedLetters.push(userGuess);
+        guessesLeft--;
+    }
+
+    // if guessesLeft are depleted alert a game over message
+    if (guessesLeft <= 0) {
+        alert("GAME OVER! You don't know your robots, bee bop, poop");
+        newGame();
+    }
+
+    // if all underscores have been matched to the robot word add a win and start a new game
+    if (underscores.join('') === robot) {
+        wins++;
+        document.querySelector("#victory-message").innerHTML = "You win! Get ready for next game!";
+        setTimeout(function() {
+            document.getElementById("victory-message").style.display = "none";
+        }, 2000)
+        setTimeout(newGame, 2100);
+    }
 
 
-var hangman = {
-    gameOn: false,
-    instructions1: document.getElementById("instructions-1"),
-    instructions2: document.getElementById("instructions-2"),
-    keyPressed: "",
-    famousRobots: ["r2d2","c3po","robocop","bender","terminator","robby","rosie","megatron","t1000","linguo","sonny","android17","android18"],
-    robot: "",
-    robotLetters: []
+    document.querySelector("#currentWord").innerHTML = underscores.join(" ");
+    document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join("~");
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    document.querySelector("#wins").innerHTML = wins;
 }
-
-// hangman.robot = Math.floor(Math.random() * this.famousRobots.length)
-
-//     movies: ["The Godfather", "The Matrix", "Captain America Civil War", "The Fox and the Hound", "Wonder", "Requiem for a Dream", "Green Street Hooligans", "Avengers Infinity War", "American Beauty", "Good Will Hunting", "Fight Club", "Guardians of the Galaxy Volume 2", "Pans Labyrinth", "Gangs of New York", "Vicky Cristina Barcelona", "300", "Big", "Deadpool", "Full Metal Jacket"],
-//     movie: "",
-//     guesses: [],
-//     charactersPressed: [],
-//     keyPressed: function (event) {
-//         var character = event.character.toLowerCase();
-//         if (this.gameOn === 0) {
-//             this.startGame();
-//         }
-//         else if (this.gameState === 1 && this.alphabet.indexOf(key) >= 0) {
-//             if (this.guesses.indexOf(key) < 0) {
-//                 this.Guess(key);
-//                 this.winCheck();
-//             }
-//         }
-//         else if (this.gameState === 2) {
-//             this.resetAll();
-//         }
-//     }
-// }
-
-// var movie = movies[Math.floor(Math.random() * movies.length)];
-// console.log(movie)
-
-// var underscores = [];
-
-// for (var i = 0; i <=movie.length;i++){
-//     // console.log(movieSelection.length)
-//     underscores.push("_")
-// }
-
-// console.log(underscores)
